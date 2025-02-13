2fb29e534ecefa2a9c8e4c1d3f1bf994bc7119bd
Wei-Jiann <ngim_wei_jian@students.edu.sg>
2025-02-13 09:25:04 +0000

    Adding more balls and a win feature.

--- script.js
+++ script.js
@@ -13,13 +13,15 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-let ball;
+let balls = [];
+let speeds = [];
 let yspeed = 1;
 let xspeed = 1;
 let ballSIZE = 80
 let Lives = 10
 let liveText; 
 let disapointment;
+let numBalls = 5;
 
 function preload() {
     // Load assets here
@@ -28,17 +30,27 @@ function preload() {
 
 function create() {
     // Initializing Game Objects here!
-    ball = this.add.sprite (WIDTH/2, HEIGHT/2, "ball");
-    ball.setDisplaySize (ballSIZE,ballSIZE) 
+    for (let i = 0; i < numBalls ; i++) {
+        let x = Phaser.Math.Between(ballSIZE, WIDTH - ballSIZE);
+        let y = Phaser.Math.Between(ballSIZE, HEIGHT - ballSIZE);
+        let ball = this.add.sprite (x,y,"ball");
+        ball.setDisplaySize (ballSIZE,ballSIZE)
 
-    ball.setInteractive ()
-    ball.on('pointerdown', function(){
+        let xspeed = Phaser.Math.floatBetween(-1,2) * (Math.random() < 0.5 ? -1 : 1);
+        let yspeed = Phaser.Math.floatBetween(-1,2) * (Math.random() < 0.5 ? -1 : 1);
+
+        balls.push(ball);
+        speeds.push([xspeed,yspeed]);
+    }
+    
+    balls.setInteractive ()
+    balls.on('pointerdown', function(){
         console.log('Ball clicked!');
         yspeed *= 1.05
-        xspeed *= 1.05
-        ball.setDisplaySize (ballSIZE, ballSIZE);
+        xspeed *= 1.09
         Lives += 1
         liveText.setText (`Lives = ${Lives}`)
+        checkGameWin();
     })
     liveText = this.add.text(600,500,`Lives : ${Lives}`, {
         fontSize: '24px',
@@ -50,14 +62,24 @@ function create() {
     })
     disapointment.setOrigin(0.5);
     disapointment.setVisible(false);
+    success = this.add.text (WIDTH/2, HEIGHT/2, 'YOU WIN!', {
+        dontSize: '64px',
+        fill: '#00ff00'
+    })
+    success.setOrigin(0.5);
+    success.setVisible(false);
 }
 
 function update() {
+    // Game logic here
     if (Lives <= 0) {
         return;
     }
 
-    // Game logic here
+    for (let i = 0; i < numBalls; i++) {
+        moveBall(balls[i], speeds[i]);
+    }
+
     let borderY = HEIGHT - ballSIZE/2
     let borderX = WIDTH - ballSIZE/2
     if (ball.y >= borderY || ball.y <= ballSIZE/2) {
@@ -74,11 +96,20 @@ function update() {
     }
     ball.y += yspeed;
     ball.x += xspeed;
-    function checkGameOver () {
-        if (Lives <= 0) {
+}
+function checkGameOver () {
+    if (Lives <= 0) {
         Lives = 0;
         liveText.setText (`Lives = 0`);
         disapointment.setVisible(true);
         ball.setVisible(false);
-    }}
+    }
+}
+function checkGameWin () {
+    if (35 <= Lives) {
+        Lives = 35
+        liveText.setText (`Lives = 35`);
+        success.setVisible(true);
+        ball.setVisible(false);
+    }
 }
